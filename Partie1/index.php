3ba6efba9b7964c74ec7e36fd99318b9cd90304d
DamColson <damien.colson@gmail.com>
2019-06-13 11:40:36 +0200

    premier commit

---
+++ Partie1/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>ExercicePhp1</title>
+        <meta charset="utf-8" />
+    </head>
+    <body>
+        <?php
+        for($i = 1;$i<8;$i++){
+            ?>
+        <p><a href=<?='Exercice'.$i.'/index.sql'?>><?= 'Exercice '.$i?></a></p>
+        <?php
+        }
+        ?>
+    </body>
+</html>
+
+
