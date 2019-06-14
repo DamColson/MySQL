832d2c4efc255c842fc078cb8124d75ae99c977d
DamColson <damien.colson@gmail.com>
2019-06-14 10:46:56 +0200

    fin de la partie 4

--- Partie1/index.sql
+++ Partie1/index.sql
@@ -17,7 +17,7 @@ CREATE TABLE languages(`id` INT NOT_NULL AUTO_INCREMENT,`language` VARCHAR(10),p
 CREATE TABLE languages(`id` INT NOT_NULL AUTO_INCREMENT,`lastname` VARCHAR(30),`firstname` VARCHAR(30),`birthDate` DATE,`address` VARCHAR(30),`firstPhoneNumber` INT,`secondPhoneNumber` INT,`address` VARCHAR(30),primary key(`id`)) ENGINE=INNODB;
 ALTER TABLE `languages` ADD COLUMN `versions` VARCHAR(20);
 ALTER TABLE `frameworks` ADD COLUMN `version` INT;
-ALTER TABLE `languages` CHANGE `versions` `version` INT;
+ALTER TABLE `languages` CHANGE `versions` `version` VARCHAR(20);
 ALTER TABLE `frameworks` CHANGE `name` `framework` VARCHAR(20);
 ALTER TABLE `frameworks` MODIFY `version` VARCHAR(10);
 USE codex
@@ -26,3 +26,17 @@ ALTER TABLE `clients` CHANGE `firstPhoneNumber` `phoneNumber` INT;
 ALTER TABLE `clients` MODIFY `phoneNumber` VARCHAR(12);
 ALTER TABLE `clients` ADD COLUMN `zipCode` VARCHAR(5);
 ALTER TABLE `clients` ADD COLUMN `city` VARCHAR(30);
+USE webdevelopment
+INSERT INTO `languages`(`language`,`version`)VALUES('JavaScript','5');
+INSERT INTO `languages`(`language`,`version`)VALUES('PHP','5,2');
+INSERT INTO `languages`(`language`,`version`)VALUES('PHP','5,4');
+INSERT INTO `languages`(`language`,`version`)VALUES('HTML','5,1');
+INSERT INTO `languages`(`language`,`version`)VALUES('JavaScript','6');
+INSERT INTO `languages`(`language`,`version`)VALUES('JavaScript','7');
+INSERT INTO `languages`(`language`,`version`)VALUES('JavaScript','8');
+INSERT INTO `languages`(`language`,`version`)VALUES('PHP','7');
+INSERT INTO `frameworks`(`framework`,`version`)VALUES('Symfony','2.8');
+INSERT INTO `frameworks`(`framework`,`version`)VALUES('Symfony','3');
+INSERT INTO `frameworks`(`framework`,`version`)VALUES('Jquery','1.6');
+INSERT INTO `frameworks`(`framework`,`version`)VALUES('Jquery','2.10');
+
